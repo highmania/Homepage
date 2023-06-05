f4f8985589a15986aece6421c159e0a0aaf35a16
highmania <117874127+highmania@users.noreply.github.com>
2023-06-05 09:59:15 +0900

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -1,16 +1,38 @@
 $(document).ready(function() {
+  //fullpage option
   $('#fullpage').fullpage({
     autoScrolling: false,
     scrollHorizontally: true,
     lockAnchors : false,
-    anchors:['first', 'second', 'third', 'forth'],
+    anchors:['first', 'second', 'third', 'forth' ,'fifth' ],
     navigation : true,
     navigationPosition :'right',
-    sectionsColor : ['#fcc', '#ffc', '#fec', '#fea'],
+    sectionsColor : ['#ddd', '#eee' , '#ddd', '#eee' , '#ddd'],
     showActiveTooltip: true,
     sectionSeletor:'.section',
     slideSelector:'.slide',
     slidesNavigation: false,
+    responsiveSlides : true,
   });
 });
-    
+$(function(){
+    $(document).on("scroll", function() {
+      let scrollTop = $(window).scrollTop();
+      if (scrollTop > 100) {
+        $("nav, .dropdown>ul li").addClass("active");
+      } else {
+        $("nav, .dropdown>ul li").removeClass("active");
+      }
+    });
+});
+
+$(function(){
+  $('.dropdown').hover(
+    function() {
+      $(this).children('ul').slideDown(500);
+    },
+    function() {
+      $(this).children('ul').slideUp(500);
+    }
+  );
+});
