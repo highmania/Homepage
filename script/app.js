46101e66599503b20aecfcd19fd12974d36ee364
highmania <117874127+highmania@users.noreply.github.com>
2023-07-10 21:42:20 +0900

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -36,3 +36,34 @@ $(function(){
     }
   );
 });
+$(function(){
+  $(".design_thumb1").click(function(){
+    $(".popup_bg1, .container1").show();
+  });
+  
+  $(".container1 .close_btn").click(function(){
+    $(".popup_bg1, .container1").hide();
+  });
+  $(".design_thumb2").click(function(){
+    $(".popup_bg2, .container2").show();
+  });
+  
+  $(".container2 .close_btn").click(function(){
+    $(".popup_bg2, .container2").hide();
+  });
+  $(".design_thumb3").click(function(){
+    $(".popup_bg3, .container3").show();
+  });
+  
+  $(".container3 .close_btn").click(function(){
+    $(".popup_bg3, .container3").hide();
+  });
+});
+
+$(function(){
+  if (".popup_bg1,.popup_bg2,.popup_bg3") {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+});
