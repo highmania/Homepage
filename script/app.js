11f70a4757206fbfd21ddd0cef7cb3ed4166a761
highmania <117874127+highmania@users.noreply.github.com>
2023-08-15 15:52:13 +0900

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,86 +1,86 @@
-$(document).ready(function() {
-  //fullpage option
-  $('#fullpage').fullpage({
-    autoScrolling: true,
-    scrollHorizontally: true,
-    lockAnchors : false,
-    anchors:['first', 'second', 'third', 'forth' ,'fifth', 'sixth', 'seventh'],
-    navigation : true,
-    navigationPosition :'right',
-    sectionsColor : ['#eee', '#fff' , '#eee', '#fff' , '#eee', '#fff', '#eee'],
-    showActiveTooltip: true,
-    sectionSeletor:'.section',
-    slideSelector:'.slide',
-    slidesNavigation: false,
-    responsiveSlides : true,
-    responsiveWidth: 900,
-  });
-});
-$(function(){
-  $('[class^="design_thumb"]').click(function(){
-    let num = parseInt($(this).attr('class').match(/\d+/));
-    $('.popup_bg' + num).stop().fadeIn();
-    $('body').on('scroll touchmove mousewheel', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      return false;
+$(function() {
+  // Fullpage option
+    $('#fullpage').fullpage({
+      autoScrolling: true,
+      scrollHorizontally: true,
+      lockAnchors : false,
+      anchors:['first', 'second', 'third', 'forth' ,'fifth', 'sixth', 'seventh'],
+      navigation : true,
+      navigationPosition :'right',
+      sectionsColor : ['#eee', '#fff' , '#eee', '#fff' , '#eee', '#fff', '#eee'],
+      showActiveTooltip: true,
+      sectionSeletor:'.section',
+      slideSelector:'.slide',
+      slidesNavigation: false,
+      responsiveSlides : true,
+      responsiveWidth: 900,
     });
-  });
 
-  $('.close_btn').click(function(){
-    let num = parseInt($(this).closest('.popup_bg').attr('class').match(/\d+/));
-    $('.popup_bg' + num).stop().fadeOut();
-    $('body').off('scroll touchmove mousewheel');
-  });
-});
+   // Code for design_thumb
+  $('[class^="design_thumb"]').click(function() {
+    // ...design_thumb click code...
+      let num = parseInt($(this).attr('class').match(/\d+/));
+      $('.popup_bg' + num).stop().fadeIn();
+      $('body').on('scroll touchmove mousewheel', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+      });
+    });
+    $('.close_btn').click(function(){
+      let num = parseInt($(this).closest('.popup_bg').attr('class').match(/\d+/));
+      $('.popup_bg' + num).stop().fadeOut();
+      $('body').off('scroll touchmove mousewheel');
+    });
 
-
-$(function(){
-    $(document).on("scroll", function() {
-      let scrollTop = $(window).scrollTop();
+  // Scroll event code
+  $(document).on("scroll", function() {
+    //...scroll event code...
+    let scrollTop = $(window).scrollTop();
       if (scrollTop > 100) {
         $("nav, .dropdown>ul li").addClass("active");
       } else {
         $("nav, .dropdown>ul li").removeClass("active");
       }
     });
-});
 
-$(function(){
+  // Dropdown hover
   $('.dropdown').hover(
     function() {
+      // ...dropdown hover in code...
       $(this).children('ul').slideDown(500);
     },
     function() {
+      // ...dropdown hover out code...
       $(this).children('ul').slideUp(500);
     }
   );
-});
 
-document.addEventListener('DOMContentLoaded', () =>{
+  // TypeIt instance code
   new TypeIt("#Introtext", {
-    strings: [""],
-    speed: 50,
-    deleteSpeed: 30,
-    loop : true
-  })
-  .pause(2000)
-  .go();
-});
+    // ...TypeIt options...
+      strings: [""],
+      speed: 50,
+      deleteSpeed: 30,
+      loop : true
+    })
+    .pause(2000)
+    .go();
 
-$(function() {
+  // Design popup click event
   for (let i = 1; i <= 7; i++) {
     $(`.design_thumb${i}`).click(function() {
+      //...design_thumb click code...
       $(`.popup_bg${i}, .container${i}`).show();
     });
 
     $(`.container${i} .close_btn`).click(function() {
+      //...close_btn click code...
       $(`.popup_bg${i}, .container${i}`).hide();
     });
   }
-});
 
-$(function(){
+  // Overflow control
   if (".popup_bg1,.popup_bg2,.popup_bg3") {
     document.body.style.overflow = 'hidden';
   } else {
